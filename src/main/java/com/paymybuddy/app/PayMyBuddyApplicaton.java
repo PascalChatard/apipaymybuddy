a31f53d039f8892660da26ea9c1afc475b5d01b3
Pascal Chatard <pchatard@aol.com>
2021-12-16 18:35:02 +0100

    Create and setup project

---
+++ src/main/java/com/paymybuddy/app/PayMyBuddyApplicaton.java
@@ -0,0 +1,12 @@
+package com.paymybuddy.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayMyBuddyApplicaton {
+	public static void main(String[] args) {
+		SpringApplication.run(PayMyBuddyApplicaton.class, args);
+	}
+
+}

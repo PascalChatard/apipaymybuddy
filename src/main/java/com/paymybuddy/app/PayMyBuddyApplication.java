353b02cb176cc4912b73a93e287929827c1cf8c7
Pascal Chatard <pchatard@aol.com>
2022-02-04 17:30:06 +0100

    nouveau projet avec MariaDB Driver

--- src/main/java/com/paymybuddy/app/PayMyBuddyApplication.java
+++ src/main/java/com/paymybuddy/app/PayMyBuddyApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PayMyBuddyApplicaton {
+public class PayMyBuddyApplication {
+
 	public static void main(String[] args) {
-		SpringApplication.run(PayMyBuddyApplicaton.class, args);
+		SpringApplication.run(PayMyBuddyApplication.class, args);
 	}
 
 }
